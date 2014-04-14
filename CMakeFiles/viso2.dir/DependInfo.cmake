f9abb67df52e957db8d359ef49a415bdba4425fa
xqjiang <yangxingjiang@gmail.com>
2014-04-13 22:16:24 -0700

    new files

---
+++ CMakeFiles/viso2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/xueqianjiang/Downloads/libviso2/src/demo.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/demo.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/filter.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/filter.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/matcher.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/matcher.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/matrix.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/matrix.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/reconstruction.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/reconstruction.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/triangle.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/triangle.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/viso.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/viso.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/viso_mono.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/viso_mono.cpp.o"
+  "/Users/xueqianjiang/Downloads/libviso2/src/viso_stereo.cpp" "/Users/xueqianjiang/Downloads/libviso2/CMakeFiles/viso2.dir/src/viso_stereo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
